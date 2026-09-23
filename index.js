be4405bbaa53062e105c846ac770552cc8bc0ee4
Juan Antony <126490331+MANOFURA@users.noreply.github.com>
2026-09-22 23:37:11 -0300

    Projeto concluído

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Olá, mundo!");
+let nome = "robinho";
+let xp = 15000;
+
+if (xp <= 1000) {
+  console.log(nome + " está na patente Ferro");
+}
+else if (xp <= 2000) {
+  console.log(nome + " está na patente Bronze");
+}
+else if (xp <= 5000) {
+  console.log(nome + " está na patente Prata");
+}
+else if (xp <= 7000) {
+  console.log(nome + " está na patente Ouro");
+}
+else if (xp <= 8000) {
+  console.log(nome + " está na patente Platina");
+}
+else if (xp <= 9000) {
+  console.log(nome + " está na patente Ascendente");
+}
+else if (xp <= 10000) {
+  console.log(nome + " está na patente Imortal");
+}
+else {
+  console.log(nome + " está na patente Radiante");
+}
